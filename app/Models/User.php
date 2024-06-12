1109a1ef846acd6243eb35d8febcc2a2a8cfda98
Celeste Gateley <celeste.gateley@gmail.com>
2024-06-12 12:53:53 +0100

    Forced email verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -17,6 +17,7 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use MustVerifyEmail;
 
     /**
      * The attributes that are mass assignable.
